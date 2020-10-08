d9ff3c6eee654c304ff13f6584d051b654a25c4d
eagleusb <git@eagleusb.com>
2020-10-08 11:38:26 +0200

    fix(records): use tomap() instead of object()

--- modules/records/main.tf
+++ modules/records/main.tf
@@ -12,7 +12,7 @@ data "aws_route53_zone" "this" {
 }
 
 resource "aws_route53_record" "this" {
-  for_each = var.create && (var.zone_id != null || var.zone_name != null) ? local.recordsets : object({})
+  for_each = var.create && (var.zone_id != null || var.zone_name != null) ? local.recordsets : tomap({})
   #  for_each = local.recordsets
 
   zone_id = data.aws_route53_zone.this[0].zone_id
