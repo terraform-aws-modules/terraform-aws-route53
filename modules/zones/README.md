61f7f90ad2f43f5d384c27466f67244c0ff2759a
Nicholas Hawkes <nicholas.hawkes@prodigygame.com>
2020-11-27 12:07:30 -0500

    feat: Adding dynamic vpc block to handle private zones (#13)

--- modules/zones/README.md
+++ modules/zones/README.md
@@ -21,7 +21,7 @@ This module creates Route53 zones.
 | Name | Description | Type | Default | Required |
 |------|-------------|------|---------|:--------:|
 | create | Whether to create Route53 zone | `bool` | `true` | no |
-| zones | Map of Route53 zone parameters | `map(any)` | `{}` | no |
+| zones | Map of Route53 zone parameters | `any` | `{}` | no |
 
 ## Outputs
 
--- modules/zones/main.tf
+++ modules/zones/main.tf
@@ -1,9 +1,18 @@
 resource "aws_route53_zone" "this" {
-  for_each = var.create ? var.zones : {}
+  for_each = var.create ? var.zones : tomap({})
 
   name          = each.key
   comment       = lookup(each.value, "comment", null)
   force_destroy = lookup(each.value, "force_destroy", false)
+
+  dynamic "vpc" {
+    for_each = length(keys(lookup(each.value, "vpc", {}))) == 0 ? [] : [lookup(each.value, "vpc", {})]
+
+    content {
+      vpc_id     = vpc.value.vpc_id
+      vpc_region = lookup(vpc.value, "vpc_region", null)
+    }
+  }
 
   tags = lookup(each.value, "tags", null)
 }
