06f4703b12e9f7ee010bdd1466c96a1981f30155
Anton Babenko <anton@antonbabenko.com>
2021-02-14 09:47:42 +0100

    Added fix for backward compatibility with single VPC

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,3 +1,20 @@
+# zones
+output "this_route53_zone_zone_id" {
+  description = "Zone ID of Route53 zone"
+  value       = module.zones.this_route53_zone_zone_id
+}
+
+output "this_route53_zone_name_servers" {
+  description = "Name servers of Route53 zone"
+  value       = module.zones.this_route53_zone_name_servers
+}
+
+output "this_route53_zone_name" {
+  description = "Name of Route53 zone"
+  value       = module.zones.this_route53_zone_name
+}
+
+# records
 output "this_route53_record_name" {
   description = "The name of the record"
   value       = module.records.this_route53_record_name
