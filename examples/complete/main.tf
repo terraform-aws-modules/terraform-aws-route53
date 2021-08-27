886c9201eff38bdf2cdf6571a0a723b8a9522d7d
chas0amx <53621461+chas0amx@users.noreply.github.com>
2021-08-27 16:44:14 +0300

    Update main.tf

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -21,6 +21,8 @@ module "zones" {
     }
 
     "private-vpc.terraform-aws-modules-example.com" = {
+      # in case than private and public zones with the same domain name
+      domain_name = "terraform-aws-modules-example.com"
       comment = "private-vpc.terraform-aws-modules-example.com"
       vpc = [
         {
