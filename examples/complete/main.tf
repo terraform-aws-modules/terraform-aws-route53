e243eedc2c6e81aa8ee1fb3b9f4ba482f5746879
Anton Babenko <anton@antonbabenko.com>
2021-08-27 15:49:38 +0200

    Fixed formatting

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -23,7 +23,7 @@ module "zones" {
     "private-vpc.terraform-aws-modules-example.com" = {
       # in case than private and public zones with the same domain name
       domain_name = "terraform-aws-modules-example.com"
-      comment = "private-vpc.terraform-aws-modules-example.com"
+      comment     = "private-vpc.terraform-aws-modules-example.com"
       vpc = [
         {
           vpc_id = module.vpc.vpc_id
